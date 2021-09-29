5851bb1f5e6c6683b578a4918a0f9575d27b2ca8
Aaron LaBeau <alabeau@gmail.com>
2021-09-29 09:30:15 -0400

    updated scripts

--- sync/docker-sync-gateway.sh
+++ sync/docker-sync-gateway.sh
@@ -4,4 +4,5 @@ docker stop sync-gateway
 echo "deleting sync-gateway  ..."
 docker rm sync-gateway
 echo "Running sync-gateway 2.8..."
-docker run -p 4984-4985:4984-4985 --network workshop --name sync-gateway -d -v `pwd`/sync-gateway-config-inventory-demo.json:/etc/sync_gateway/sync_gateway.json couchbase/sync-gateway:2.8.2-enterprise  -adminInterface :4985 /etc/sync_gateway/sync_gateway.json
+docker run -p 4984-4985:4984-4985 --network workshop --name sync-gateway -d -v `pwd`/sync-gateway-config-inventory-demo.json:/etc/sync_gateway/sync_gateway.json couchbase/sync-gateway:2.8.2-enterprise -adminInterface :4985 /etc/sync_gateway/sync_gateway.json
+
